e1acef74c19529160700aa5980e337675b45fc6a
stark-4-11 <starklakshaycoder@gmail.com>
2025-03-24 20:21:52 +0530

    Updated [describe changes]

--- src/components/page3/ProjectCard.jsx
+++ src/components/page3/ProjectCard.jsx
@@ -52,12 +52,13 @@ function HomePage() {
 
           <div className='Buttons flex space-x-25 mt-8 justify-center items-center'>
             <div className="group relative">
+              <a href="https://movie-verse-beta.vercel.app/">
               <button>
                 <FaLink
                   size={20}
                   className="text-white w-8 group-hover:text-blue-500 hover:scale-125 duration-200"
                 />
-              </button>
+              </button></a>
               <span
                 className="absolute -top-14 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 px-3 rounded-lg border border-gray-300 bg-black py-2 text-sm font-bold shadow-md transition-all duration-300 ease-in-out group-hover:scale-100"
               >
@@ -66,7 +67,7 @@ function HomePage() {
             </div>
 
             <div className="group relative">
-              <button>
+              <a href="https://github.com/stark-4-11/Portfolio-website"><button>
                 <svg
                   strokeLinejoin="round"
                   strokeLinecap="round"
@@ -78,7 +79,8 @@ function HomePage() {
                 >
                   <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                 </svg>
-              </button>
+                
+              </button></a>
               <span
                 className="absolute -top-14 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 px-3 rounded-lg border border-gray-300 bg-black py-2 text-sm font-bold shadow-md transition-all duration-300 ease-in-out group-hover:scale-100"
               >
